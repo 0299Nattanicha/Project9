489b18a8314475859a1a5a50c9189f648c122810
NATTANICHA SENTHA <63010299@KMITL.AC.TH>
2020-09-03 15:23:21 +0700

    Add project files.

---
+++ Project9/Source.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int max= -2000000000,min=2000000000,n;
+	scanf("%d" ,& n);
+	int a;
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a);
+		if (a > max)
+		{
+			max = a;
+		}
+		if (a < min)
+		{
+			min = a;
+		}
+	}
+	printf("%d\n%d", min, max);
+}
